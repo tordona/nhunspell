c07d67f7479ca7c27a3523f6cb48955d9ba713e5
tmaierhofer <none@none>
2012-05-29 11:45:58 +0200

    - Powershell Post Build event - Removing Release files from Unit Test

---
+++ NHunspell/UnitTests/PostBuild.ps1
@@ -0,0 +1,14 @@
+﻿# Post Build Script
+Write-Host "Args:" $Args.Length
+foreach( $Arg in $Args ) { Write-Host "Arg: $Arg" }
+if( $Args.Length -ne 4 ) { Throw "Wrong Number of Arguments: $Args.Length"  }
+
+# Setting Parameters from Arguments
+$ConfigurationName = $Args[0]
+$PlatformName = $Args[1]
+$ProjectDir = $Args[2]
+$TargetDir = $Args[3]
+
+Copy-Item $ProjectDir\Hunspellx86.dll  $TargetDir
+Copy-Item $ProjectDir\Hunspellx64.dll  $TargetDir
+
