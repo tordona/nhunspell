d6735ace8663f3f1bf2dd20ef1e34c8ac225ec5a
tmaierhofer <none@none>
2014-09-03 12:27:16 +0200

    Samples Build Process

---
+++ NHunspellSamples/PackBuild.ps1
@@ -0,0 +1,19 @@
+﻿Stop-Process -Name CSharpConsoleSamples.vshost -ErrorAction Ignore
+remove-item CSharpConsoleSamples\bin -Recurse -Force -ErrorAction Ignore
+remove-item CSharpConsoleSamples\obj -Recurse -Force -ErrorAction Ignore
+
+Stop-Process -Name VisualBasicConsoleSampels.vshost -ErrorAction Ignore
+remove-item VisualBasicConsoleSampels\bin -Recurse -Force -ErrorAction Ignore
+remove-item VisualBasicConsoleSampels\obj -Recurse -Force -ErrorAction Ignore
+
+remove-item WebSampleApplication\bin -Recurse -Force -ErrorAction Ignore
+remove-item WebSampleApplication\obj -Recurse -Force -ErrorAction Ignore
+
+
+$packageZipFile = $context.SolutionArtifactsLocation + "\NHunspellSamples." + $context.PackageVersion + ".zip"
+
+Get-ChildItem CSharpConsoleSamples -Recurse | Export-Zip $packageZipFile
+Get-ChildItem VisualBasicConsoleSampels -Recurse | Export-Zip $packageZipFile -Append
+Get-ChildItem WebSampleApplication -Recurse | Export-Zip $packageZipFile -Append
+Get-ChildItem packages -Recurse | Export-Zip $packageZipFile -Append
+Export-Zip $packageZipFile -EntryFile NHunspellSamples.sln -Append
---
+++ NHunspellWeb/index.php
@@ -0,0 +1,181 @@
+﻿ <?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: http://www.crawler-lib.net/nhunspell");
+?> 
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head><title>NHunspell: C#/.NET free Spell Checker, Hyphenator,
+Thesaurus. Hunspell spelling corrector and hyphenation for C# and
+Visual Basic</title>
+
+<meta name="description" content="Spellchecker, Hyphenation and Thesaurus for the Microsoft .NET Framework. Hunspell the Open Office Spell Checker" />
+<meta name="keywords" content="Hunspell, NHunspell, Spell, Check, Hyphenation, Hyphenator, Thesaurus, Synonyms, .NET, C#, VB" />
+<meta http-equiv="content-language" content="en" />
+<meta name="robots" content="all" />
+<link rel="shortcut icon" href="/favicon.png" type="image/png" />
+<link rel="icon" href="/favicon.png" type="image/png" />
+<meta name="verify-v1" content="RmkbP0CPZ/QtQlxAgBVsXdvd5by1U31Qv2PkrjFt5X8=" />
+<meta name="y_key" content="9f0ee97dc76cda0d" />
+<meta name="google-site-verification" content="QS4icAh7knLfeD4VdSvHillysggGvoIrtJ0a7KUY85g" />
+<!--[if lt IE 8]> <script type="text/javascript" src="/Script/IE8.js" ></script> <![endif]--><!--[if IE]> <script type="text/javascript" src="/Script/excanvas.js" ></script> <![endif]-->
+<script src="/Script/JQuery/JQuery.js" type="text/javascript"></script>
+<script src="/Script/JQuery/JQuery.utilities.js" type="text/javascript"></script>
+<script src="/Script/JQuery/jquery.dimensions.pack.js" type="text/javascript"></script>
+<script src="/Script/JQuery/JQuery.backgroundCanvas.js" type="text/javascript"></script>
+<script src="/Script/JQuery/JQuery.layoutHelpers.js" type="text/javascript"></script>
+<script src="/Script/ImageFormFlow.js" type="text/javascript"></script>
+<script src="/Script/Background.js" type="text/javascript"></script>
+<script src="/Script/Layout.js" type="text/javascript"></script>
+<script type="text/javascript">
+</script>
+<link href="App_Themes/RibbonBlue/Body.css" type="text/css" rel="stylesheet" />
+<link href="App_Themes/RibbonBlue/Content.css" type="text/css" rel="stylesheet" />
+<link href="App_Themes/RibbonBlue/Footer.css" type="text/css" rel="stylesheet" />
+<link href="App_Themes/RibbonBlue/Layout.css" type="text/css" rel="stylesheet" />
+<link href="App_Themes/RibbonBlue/Navigation.css" type="text/css" rel="stylesheet" />
+<link href="App_Themes/RibbonBlue/News.css" type="text/css" rel="stylesheet" />
+<link type="text/css" rel="stylesheet" href="/Script/Syntax/styles/shCore.css" />
+<link type="text/css" rel="stylesheet" href="/Script/Syntax/styles/shThemeDefault.css" />
+</head> 
+
+<body>
+<div id="LayoutContainer">
+<div id="Navigation"><span class="logo"><a title="NHunspell Logo - Spell Check - Thesaurus - Hyphenation" href="/">
+<img src="/Images/NHunspell-Logo.png" alt="NHunspell Logo" /></a></span>
+<h1>Free Spell-Checker, Hyphenation and Thesaurus for .NET
+(C#,Visual Basic,...)</h1>
+</div>
+<div id="ContentLayoutContainer">
+<div id="Content">
+<div class="Article">
+<h1>New Homepage for NHunspell</h1>
+<p>
+To provide better support we decided to move the NHunspell homepage to the Crawler-Lib website which is maintained and monitored regulary by the developers of NHunspell.
+The project is still open source and free. The sourcecode remains on Sourceforge, only the Homepage and the support home changes. Please update your bookmarks.<br />  
+<a href="http://www.crawler-lib.net/nhunspell">New NHunspell Homepage</a><br />
+<a href="http://www.crawler-lib.net/boards/forum/5/nhunspell">New NHunspell Support Home</a>
+</p>
+</div>
+
+<div class="Article">
+<h1>Spell Checker, Hyphenation and Thesaurus: NHunspell</h1>
+<p>NHunspell brings the spell checking, hyphenation and thesaurus
+to the Microsoft® .NET Framework.
+NHunspell is C# library and wraps native libraries for Hunspell, Hyphen
+and MyThes. One design goal of this library and wrapper is to keep the
+source code of the included libraries as unmodified as possible.
+New versions of the base libraries can therefore easily adopted to
+NHunspell.</p>
+<p>The integrated libraries are used in OpenOffice and they work
+with the dictionaries published on OpenOffice.org.</p>
+<h2>License</h2>
+<p>NHunspell is licensed under: GPL/LGPL/MPL. Free use in
+commercial applications is permitted according to the LGPL and MPL
+licenses. Your commercial application can link against the NHunspell
+DLLs.</p>
+</div>
+<div class="Article">
+<h1>Hunspell</h1>
+<p>The spell checker library <a href="http://hunspell.sourceforge.net" title="Hunspell spell checker library">Hunspell</a>
+is a state of the art spell checker for languages with complex word
+compunding and rich morphology. It was written by László Németh for
+spell checking of the Hungarian language and can be used with utf8
+encoded unicode directories. Hunspell is based on MySpell and can use
+MySpell directories too. </p>
+<p>
+Hunspell is the default spell checker of OpenOffice, Mozilla
+Thunderbird and Firefox, Google Chrome and the Apple MAC OS/X operating
+system since version 10.6 "Snow Leopard".
+Hunspell is based an MySpell and can use the MySpell dictionaries. It
+is an alternative to GNU Aspell, ISpell, PSpell and Enchant
+</p>
+</div>
+<div class="Article">
+<h1>Hyphen</h1>
+The hyphenation library Hyphen is part of the Hunspell project written
+by László Németh. It is a state of the art hyphenator. </div>
+<div class="Article">
+<h1>MyThes</h1>
+The thesaurus MyThes is part of the Hunspell project. It can be used to
+find synonyms for words.
+</div>
+</div>
+
+
+<div id="News">
+<div id="NewsBox"><span>News</span>
+
+<blockquote>
+<div class="date">2014/07/19</div>
+<h3>New Homepage</h3>
+<p>NHunspell has a new homepage.<br />
+<a href="http://www.crawler-lib.net/nhunspell">New NHunspell Homepage</a>
+</p>
+</blockquote>
+
+<blockquote>
+<div class="date">2013/05/12</div>
+<h3>NHunspell 1.1.1 RTM</h3>
+<p>Releasing NHunspell 1.1.0 RTM</p>
+<p>Hunspell 1.3.2, Hyphen 2.8.6</p>
+<p>Patches till 2013-05-12</p>
+</blockquote>
+
+<blockquote>
+<div class="date">2013/03/18</div>
+<h3>NHunspell 1.1.0 RTM</h3>
+<p>Releasing NHunspell 1.1.0 RTM</p>
+<p>Hunspell 1.3.2, Hyphen 2.8.6</p>
+<p>Patches till 2013-03-14</p>
+</blockquote>
+
+<blockquote>
+<div class="date">2012/06/04</div>
+<h3>NHunspell 1.0.0 RTM</h3>
+<p>Releasing NHunspell 1.0.0 RTM</p>
+<p>Hunspell 1.3.2, Hyphen 2.8.3</p>
+</blockquote>
+
+<blockquote>
+<div class="date">2011/11/14</div>
+<h3>NHunspell 0.9.9</h3>
+<p>Releasing NHunspell 0.9.9</p>
+<p>Update to Hunspell 1.3.2, Hyphen 2.8.3</p>
+</blockquote>
+
+<blockquote>
+<div class="date">2011/8/10</div>
+<h3>NHunspell 0.9.8</h3>
+<p>Releasing NHunspell 0.9.8</p>
+<p>Update to Hunspell 1.3.2, Hyphen 2.8</p>
+</blockquote>
+
+<blockquote>
+<div class="date">2010/12/1</div>
+<h3>NHunspell 0.9.7</h3>
+<p>Releasing NHunspell 0.9.7</p>
+<p>Update to Hunspell 1.2.12, Hyphen 2.6</p>
+</blockquote>
+</div>
+</div>
+</div>
+
+<div id="Footer">
+<p>© 2009-2013 by Thomas Maierhofer and all contributing Authors.&nbsp; </p>
+</div>
+</div>
+<!-- Syntax Highlighter --><script type="text/javascript" src="/Script/Syntax/scripts/shCore.js"></script><script type="text/javascript" src="/Script/Syntax/scripts/shBrushCpp.js"></script><script type="text/javascript" src="/Script/Syntax/scripts/shBrushCSharp.js"></script><script type="text/javascript">
+SyntaxHighlighter.config.clipboardSwf = '/Script/Syntax/scripts/clipboard.swf';
+SyntaxHighlighter.all();
+</script><!-- Google Analytics Tracking --><script src="http://www.google-analytics.com/ga.js" type="text/javascript"></script>
+
+
+
+
+
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-6279000-2");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+</body></html>
